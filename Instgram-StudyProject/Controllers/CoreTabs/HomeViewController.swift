7cca728e0af7ea742e8c189b8bbd27406726cee5
MinaHanna <Mina.Hanna@itworxedu.com>
2022-02-06 16:26:06 +0200

    Instgram Study app first push

---
+++ Instgram-StudyProject/Controllers/CoreTabs/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instgram-StudyProject
+//
+//  Created by Mina Hanna on 06/02/2022.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Instgram-StudyProject/Controllers/Others/OnBoarding/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginViewController.swift
+//  Instgram-StudyProject
+//
+//  Created by Mina Hanna on 06/02/2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+//MARK: -
+
+}
