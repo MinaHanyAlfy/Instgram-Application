6235944dd8bb8b3a72621bbcbf5827a2b2b7d612
Ali Samy <ali.samy@itworxedu.com>
2022-02-24 16:38:14 +0200

    half of Part 7

--- Instgram-StudyProject/Controllers/Others/General/EditProfileViewController.swift
+++ Instgram-StudyProject/Controllers/Others/General/EditProfileViewController.swift
@@ -7,10 +7,26 @@
 
 import UIKit
 
+struct EditProfileModel{
+    let label: String
+    let placeHolder: String
+    var value: String?
+}
+
 class EditProfileViewController: UIViewController {
     
+    private let tableView : UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        return tableView
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(tableView)
+        tableView.tableHeaderView = headerTableView()
+        tableView.delegate = self
+        tableView.dataSource = self
         view.backgroundColor = .systemBackground
         navigationItem.rightBarButtonItem  = UIBarButtonItem(title: "Save",
                                                              style: .done,
@@ -20,7 +36,51 @@ class EditProfileViewController: UIViewController {
                                                             style: .plain,
                                                             target: self,
                                                             action: #selector(didTapCancel))
+        configureModels()
         // Do any additional setup after loading the view.
+    }
+    private var models = [[EditProfileModel]]()
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        tableView.frame = view.bounds
+    }
+    
+    private func configureModels(){
+        //name ,username, url, website, bio
+        let section1Labels =  ["Name","Username","Bio"]
+        var section1 = [EditProfileModel]()
+        for label in section1Labels {
+            let model = EditProfileModel(label: label, placeHolder: "Enter \(label)...", value: nil)
+            section1.append(model)
+        }
+        models.append(section1)
+        //email, phone, gender
+        var section2 = [EditProfileModel]()
+        let section2Labels = ["Email","Phone","Gender"]
+        for label in section2Labels {
+            let model = EditProfileModel(label: label, placeHolder: "Enter \(label)...", value: nil)
+            section2.append(model)
+        }
+        models.append(section2)
+    }
+    private func headerTableView () -> UIView{
+        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: view.width, height: view.height/4.0).integral)
+        let size = headerView.height/1.5
+        let profilePhotoButton = UIButton(frame: CGRect(x: (view.width-size)/2, y: (headerView.height-size)/2, width: size, height: size))
+        
+        headerView.addSubview(profilePhotoButton)
+        profilePhotoButton.layer.masksToBounds = true
+        profilePhotoButton.layer.cornerRadius = size/2.0
+        profilePhotoButton.addTarget(self, action: #selector(didTapChaneProfilePicture), for: .touchUpInside)
+        profilePhotoButton.tintColor = .label
+        profilePhotoButton.setImage(UIImage(systemName: "person.circle"), for: .normal)
+        profilePhotoButton.imageView?.contentMode = .scaleToFill
+        profilePhotoButton.layer.borderWidth = 1.0
+        profilePhotoButton.layer.borderColor = UIColor.secondarySystemBackground.cgColor
+
+        
+        return headerView
     }
     
     @objc private func didTapSave(){
@@ -49,5 +109,29 @@ class EditProfileViewController: UIViewController {
         present(actionSheet, animated: true, completion: nil)
     }
     
+    
+}
+extension EditProfileViewController: UITableViewDelegate, UITableViewDataSource{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return models.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models[section].count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model = models[indexPath.section][indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = model.label
+        
+        return cell
+    }
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        guard section == 1 else {
+            return nil
+        }
+        return "Private Information"
+    }
     
 }
